f261daf821fc48f45d47de95ada7faa5c841b681
MuhammedKB <mohamad.kabbani@ogr.sakarya.edu.tr>
2023-02-14 10:34:34 +0300

    Add Main Repository

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        public void Add(T t);
+        public void Update(T t);
+        public void Delete(T t);
+        public T Get(int id);
+        public List<T> GetAll();
+    }
+}
---
+++ DataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        Context _context;
+
+        public Repository()
+        {
+            _context = new Context();
+        }
+
+        public void Add(T t)
+        {
+            _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
