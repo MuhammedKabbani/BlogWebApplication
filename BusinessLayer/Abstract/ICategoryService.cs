6a0695097d36e8d26d249a34dd7691c69269dda8
MuhammedKB <mohamad.kabbani@ogr.sakarya.edu.tr>
2023-02-14 11:09:28 +0300

    Add Repositories And add the first Service to Business Layer

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        public void AddCategory(Category c);
+        public void DeleteCategory(Category c);
+        public void UpdateCategory(Category c);
+        public Category GetCategoryById(int id);
+        public List<Category>GettAllCategories();
+    }
+}
---
+++ BusinessLayer/Concrate/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrate
+{
+    public class CategoryService : ICategoryService
+    {
+        CategoryRepository _repo;
+        public CategoryService()
+        {
+            _repo = new CategoryRepository();
+
+        }
+        public void AddCategory(Category c)
+        {
+            _repo.Add(c);
+        }
+
+        public void DeleteCategory(Category c)
+        {
+            _repo.Delete(c);
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public List<Category> GettAllCategories()
+        {
+            return _repo.GetAll();
+        }
+
+        public void UpdateCategory(Category c)
+        {
+            _repo.Update(c);
+        }
+    }
+}
